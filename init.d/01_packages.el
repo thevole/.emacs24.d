0cdec182da24f6867c145ba5fca9e805846913b9
Martin Volerich <martin@volerich.com>
2013-01-11 16:58:42 -0500

    Added required packages

--- init.d/01_packages.el
+++ init.d/01_packages.el
@@ -1,6 +1,16 @@
 (require 'package)
-(add-to-list 'package-archives 
-            '("marmalade" . "http://marmalade-repo.org/packages/"))
+
 (add-to-list 'package-archives
-            '("ELPA" . "http://tromey.com/elpa/"))
+	     '("gnu" . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives
+	     '("Marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives
+	     '("Tromey" . "http://tromey.com/elpa/") t)
 (package-initialize)
+
+(setq mbv-required-packages 
+      (list 'xml-rpc 'magit 'gh 'inf-ruby))
+(dolist (package mbv-required-packages)
+  (when (not (package-installed-p package))
+    (package-refresh-contents)
+    (package-install package)))
