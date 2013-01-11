aa363a6fc5147a5212461c1394168ba05e4fbd79
Martin Volerich <martin@volerich.com>
2013-01-11 09:17:41 -0500

    Customized package archives

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
+ '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/") ("Tromey" . "http://tromey.com/elpa/"))))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
