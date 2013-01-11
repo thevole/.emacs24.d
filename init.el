0cab29842df8f57e3169f15dbcb6eac9c98b1d28
Martin Volerich <martin@volerich.com>
2013-01-11 08:41:26 -0500

    Customized backup file folder

--- init.el
+++ init.el
@@ -2,6 +2,11 @@
 (setq mbv-emacs-config-dir
       (file-name-directory mbv-emacs-init-file))
 
+(setq user-emacs-directory mbv-emacs-config-dir)
+
+(setq backup-directory-alist
+      (list (cons "." (expand-file-name "backup" user-emacs-directory))))
+
 ;; Set up 'custom' system
 (setq custom-file (expand-file-name "emacs-customizations.el" mbv-emacs-config-dir))
 (load custom-file)
