fa6d6b628cc9455a72fd3e643624b8c6efe54b95
Martin Volerich <martin@volerich.com>
2013-01-11 09:51:09 -0500

    Added ruby line number mode and column mode global

--- init.el
+++ init.el
@@ -26,3 +26,9 @@
 (autoload 'Lorem-ipsum-insert-paragraphs "lorem-ipsum" "" t)
 (autoload 'Lorem-ipsum-insert-sentences "lorem-ipsum" "" t)
 (autoload 'Lorem-ipsum-insert-list "lorem-ipsum" "" t)
+
+;; Line numbers hook
+(add-hook 'mbv-code-modes-hook
+	  (lambda () (linum-mode 1)))
+(add-hook 'ruby-mode-hook
+	  (lambda () (run-hooks 'mbv-code-modes-hook)))
