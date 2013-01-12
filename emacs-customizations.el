0a2d4f519e5ef72c6fdd0f5760259b306bd3ce44
Martin Volerich <martin@volerich.com>
2013-01-12 11:08:26 -0500

    More customizations (viget.com)

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -3,10 +3,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(delete-selection-mode t)
  '(ido-enable-flex-matching t)
  '(ido-mode (quote both) nil (ido))
+ '(inhibit-startup-screen t)
+ '(make-backup-files nil)
  '(package-archives (quote (("gnu" . "http://elpa.gnu.org/packages/") ("marmalade" . "http://marmalade-repo.org/packages/") ("Tromey" . "http://tromey.com/elpa/"))))
- '(tool-bar-mode nil))
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(tooltip-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ init.d/03_defaults.el
@@ -0,0 +1,4 @@
+(setq auto-save-default nil)
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode nil)
+(set-fringe-style -1)
