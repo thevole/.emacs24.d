d009693f708135e17e1c12dfa331549fdee37764
Martin Volerich <martin@volerich.com>
2013-01-17 09:00:38 -0500

    Removed redundant commented keymap code

--- init.d/ruby.el
+++ init.d/ruby.el
@@ -1,11 +1,6 @@
 (add-hook 'ruby-mode-hook
           (lambda () (run-hooks 'mbv-code-modes-hook)))
 
-;; (eval-after-load 'ruby-mode
-;;   '(progn
-;;      (require 'rcodetools)
-;;      (define-key ruby-mode-map (kbd "C-c C-c") 'xmp)))
-
 (defun mbv-add-rubyxmp ()
   (require 'rcodetools)
   (local-set-key (kbd "C-c C-c") 'xmp))
