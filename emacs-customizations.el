f2801774cd0da8708b2bbcb238f3f77c3df29b44
Martin Volerich <martin@volerich.com>
2013-01-17 15:23:10 -0500

    Added solarized theme

--- emacs-customizations.el
+++ emacs-customizations.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(custom-safe-themes (quote ("71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default)))
+ '(custom-safe-themes (quote ("501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" "72cc9ae08503b8e977801c6d6ec17043b55313cda34bcf0e6921f2f04cf2da56" "71b172ea4aad108801421cc5251edb6c792f3adbaecfa1c52e94e3d99634dee7" default)))
  '(delete-by-moving-to-trash t)
  '(delete-selection-mode t)
  '(electric-indent-mode t)
--- init.d/10_themes.el
+++ init.d/10_themes.el
@@ -1,11 +1,13 @@
 (set-frame-font "Monaco-15")
-(load-theme 'wombat t)
+;; (load-theme 'wombat t)
 ;; Zenburn is a low-contrast theme, good for long stretches of
 ;; coding. Also uses the newer theming system.
-;; (add-to-list 'el-get-sources
-;;             '(:name zenburn-theme :type elpa))
-;; (el-get 'sync '(zenburn-theme))
+(add-to-list 'el-get-sources
+            '(:name zenburn-theme :type elpa))
+(el-get 'sync '(zenburn-theme))
+(add-to-list 'el-get-sources
+            '(:name solarized-theme :type elpa))
+(el-get 'sync '(solarized-theme))
 
 ;(add-hook 'after-init-hook 'color-theme-molokai)
-;;(add-hook 'after-init-hook (lambda () (load-theme 'zenburn)))
-
+(add-hook 'after-init-hook (lambda () (load-theme 'solarized-dark)))
