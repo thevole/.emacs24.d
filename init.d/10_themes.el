f975121b31e3c1b7a84c364395858abf270cd5cd
Martin Volerich <martin@volerich.com>
2013-01-17 15:55:51 -0500

    Switched to standard wheatgrass theme

--- init.d/10_themes.el
+++ init.d/10_themes.el
@@ -1,13 +1,14 @@
 (set-frame-font "Monaco-15")
-;; (load-theme 'wombat t)
+(load-theme 'wheatgrass t)
+
 ;; Zenburn is a low-contrast theme, good for long stretches of
 ;; coding. Also uses the newer theming system.
-(add-to-list 'el-get-sources
-            '(:name zenburn-theme :type elpa))
-(el-get 'sync '(zenburn-theme))
-(add-to-list 'el-get-sources
-            '(:name solarized-theme :type elpa))
-(el-get 'sync '(solarized-theme))
 
-;(add-hook 'after-init-hook 'color-theme-molokai)
-(add-hook 'after-init-hook (lambda () (load-theme 'solarized-dark)))
+;; (add-to-list 'el-get-sources
+;;             '(:name zenburn-theme :type elpa))
+;; (el-get 'sync '(zenburn-theme))
+
+;; (add-to-list 'el-get-sources
+;;             '(:name solarized-theme :type elpa))
+;; (el-get 'sync '(solarized-theme))
+;; (add-hook 'after-init-hook (lambda () (load-theme 'solarized-dark)))
