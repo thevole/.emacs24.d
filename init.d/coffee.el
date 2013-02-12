ec4e2cc2bf95767256f38c26246fe128b0825ae9
Martin Volerich <martin@volerich.com>
2013-02-12 17:33:13 -0500

    Better mode customization

--- init.d/coffee.el
+++ init.d/coffee.el
@@ -1,2 +1,8 @@
-;; Tab width
-(setq coffee-tab-width 2)
+;; Line numbers & tabwidth & electric indent
+(add-hook 'mbv-coffee-mode-hook
+          (lambda () (linum-mode 1))
+          (setq coffee-tab-width 2)
+          (electric-indent-mode 0))
+
+(add-hook 'coffee-mode-hook 'mbv-coffee-mode-hook)
+(add-hook 'haml-mode-hook 'mbv-coffee-mode-hook)
--- init.d/ruby.el
+++ init.d/ruby.el
@@ -2,8 +2,14 @@
   (require 'rcodetools)
   (local-set-key (kbd "C-c C-c") 'xmp))
 
-(add-hook 'ruby-mode-hook 'mbv-add-rubyxmp)
-
 (defun ri-bind-key ()
   (local-set-key (kbd "<f1>") 'yari))
+
+
+(add-hook 'ruby-mode-hook
+          (lambda ()
+            (electric-indent-mode 1)))
+
+(add-hook 'ruby-mode-hook 'mbv-add-rubyxmp)
+
 (add-hook 'ruby-mode-hook 'ri-bind-key)
