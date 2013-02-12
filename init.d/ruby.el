3237024fef9c6b1eced0c3804a83bccfbda48016
Martin Volerich <martin@volerich.com>
2013-02-12 17:38:22 -0500

    More ruby & css customizations

--- init.d/ruby.el
+++ init.d/ruby.el
@@ -13,3 +13,19 @@
 (add-hook 'ruby-mode-hook 'mbv-add-rubyxmp)
 
 (add-hook 'ruby-mode-hook 'ri-bind-key)
+
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
+(add-hook 'ruby-mode-hook '(lambda ()
+                             (setq ruby-deep-arglist t)
+                             (setq ruby-deep-indent-paren nil)
+                             (setq c-tab-always-indent nil)
+                             (require 'inf-ruby)
+                             (require 'ruby-compilation)
+                             ))
+
+(add-hook 'css-mode-hook '(lambda ()
+                            (setq css-indent-level 2)
+                            (setq css-indent-offset 2)))
