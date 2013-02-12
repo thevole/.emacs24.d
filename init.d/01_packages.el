c7d720a7dba2ce59b007b2b6692c39efad35de80
Martin Volerich <martin@volerich.com>
2013-02-12 17:32:39 -0500

    Switched package managed to melpa

--- init.d/01_packages.el
+++ init.d/01_packages.el
@@ -1,108 +1,12 @@
-(setq el-get-dir (expand-file-name "el-get" user-emacs-directory))
-(add-to-list 'load-path
-             (expand-file-name "el-get/el-get" user-emacs-directory))
+(require 'package)
+;; (add-to-list 'package-archives
+;;          '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/"))
+;; (add-to-list 'package-archives
+;;              '("gnu" . "http://elpa.gnu.org/"))
+;; (add-to-list 'package-archives
+;;              '("tromey" . "http://tromey.com/elpa/"))
+(package-initialize)
 
-(setq el-get-user-package-directory
-      (expand-file-name "init-files" el-get-dir))
-
-
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-(defun ruby-mode-hook ()
-   (autoload 'ruby-mode "ruby-mode" nil t)
-   (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
-   (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
-   (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
-   (add-to-list 'auto-mode-alist '("\\.rake\\'" . ruby-mode))
-   (add-hook 'ruby-mode-hook '(lambda ()
-                               (setq ruby-deep-arglist t)
-                               (setq ruby-deep-indent-paren nil)
-                               (setq c-tab-always-indent nil)
-                               (require 'inf-ruby)
-                               (require 'ruby-compilation)
-                               )))
-
-(defun css-mode-hook ()
-  (autoload 'css-mode "css-mode" nil t)
-  (add-hook 'css-mode-hook '(lambda ()
-                              (setq css-indent-level 2)
-                              (setq css-indent-offset 2))))
-
-(setq el-get-sources
-      '((:name rhtml-mode
-               :type git
-               :url "https://github.com/eschulte/rhtml.git"
-               :features rhtml-mode)
-        (:name coffee-mode
-               :type git
-               :url "https://github.com/defunkt/coffee-mode.git"
-               :features coffee-mode)
-        (:name ruby-mode
-               :type elpa
-               :features ruby-mode
-               :after (progn (ruby-mode-hook)))
-        (:name ruby-compilation :type elpa)
-        (:name yaml-mode
-               :type git
-               :url "http://github.com/yoshiki/yaml-mode.git"
-               :features yaml-mode)
-        (:name css-mode
-               :type elpa
-               :after (progn (css-mode-hook)))
-        (:name rspec-mode
-               :type git
-               :url "https://github.com/pezra/rspec-mode.git"
-               :features rspec-mode)
-        (:name cucumber
-               :type git
-               :url "https://github.com/michaelklishin/cucumber.el.git"
-               :features feature-mode)
-        (:name scss-mode
-               :type git
-               :url "https://github.com/antonj/scss-mode.git"
-               :features scss-mode)
-        (:name magit
-               :type git
-               :url "https://github.com/magit/magit.git"
-               :features magit)
-        (:name yari
-               :type git
-               :url "https://github.com/hron/yari.el.git"
-               :load "yari.el")
-        (:name rinari
-               :type git
-               :url "https://github.com/eschulte/rinari.git"
-               :local "rinari.el")
-        (:name wrap-region
-               :type git
-               :url "https://github.com/rejeep/wrap-region.git"
-               :features wrap-region)
-        (:name expand-region
-               :type git
-               :url "https://github.com/magnars/expand-region.el.git"
-               :features expand-region)
-        (:name jump-char
-               :type git
-               :url "https://github.com/lewang/jump-char.git"
-               :features jump-char)
-        (:name ace-jump
-               :type git
-               :url "https://github.com/winterTTr/ace-jump-mode.git"
-	       :load "ace-jump-mode.el")
-))
-
-     (setq mbv-packages
-           (append
-            '(el-get
-              fill-column-indicator
-              haml-mode
-              coffee-mode)
-            (mapcar 'el-get-source-name el-get-sources)))
-
-
-(el-get 'sync mbv-packages)
+(require 'wrap-region)
