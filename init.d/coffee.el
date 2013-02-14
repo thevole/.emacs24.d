52665515ff65fc1e804a945d0887d7d231a86231
Martin Volerich <martin@volerich.com>
2013-02-14 17:16:21 -0500

    plain // comments for scss files

--- init.d/coffee.el
+++ init.d/coffee.el
@@ -8,3 +8,8 @@
 (add-hook 'haml-mode-hook
           (lambda ()
             (electric-indent-mode 0)))
+
+(add-hook 'scss-mode-hook
+          (lambda ()
+            (setq comment-start "//")
+            (setq comment-end "")))
